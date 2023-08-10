b3a7511e1c464cee133cfd38db9ad4d80de95ce4
Wonhee Son <dnjsgml1230@gmail.com>
2023-08-11 01:35:08 +0900

    fix: 검증 실패 시 사용자가 필드에 입력한 값 유지

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,7 +45,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         /*
@@ -86,11 +86,93 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        /*
+        상품 추가 폼 필드 별 검증 로직
+         */
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000원에서 1,000,000원 사이의 값이야 합니다."));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999개까지 설정할 수 있습니다."));
+        }
+
+        //상품 추가 폼의 특정 필드가 아닌 복합 룰 검증 로직
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 결과가 10,000원 이상이어야 합니다. 현재 " + resultPrice + "원입니다."));
+            }
+        }
+
+        //검증을 거치지 못한 필드 또는 복합 룰이 하나라도 존재하는 경우 다시 입력 폼 렌더링
+        if (bindingResult.hasErrors()) {
+            log.info("errors={} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //검증을 성공적으로 거쳤을 때의 실행 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
         return "validation/v2/editForm";
+    }
+
+    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        /*
+        상품 추가 폼 필드 별 검증 로직
+         */
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000원에서 1,000,000원 사이의 값이야 합니다."));
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, null, null, "수량은 최대 9,999개까지 설정할 수 있습니다."));
+        }
+
+        //상품 추가 폼의 특정 필드가 아닌 복합 룰 검증 로직
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null, "가격 * 수량의 결과가 10,000원 이상이어야 합니다. 현재 " + resultPrice + "원입니다."));
+            }
+        }
+
+        //검증을 거치지 못한 필드 또는 복합 룰이 하나라도 존재하는 경우 다시 입력 폼 렌더링
+        if (bindingResult.hasErrors()) {
+            log.info("errors={} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //검증을 성공적으로 거쳤을 때의 실행 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
     }
 
     @PostMapping("/{itemId}/edit")
