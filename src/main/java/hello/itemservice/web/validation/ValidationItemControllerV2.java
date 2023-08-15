3f079eb87a09da44f07865935f1e4bb1f18e9fc6
Wonhee Son <dnjsgml1230@gmail.com>
2023-08-15 13:37:57 +0900

    feat: Validator 구현 클래스에 검증 로직 분리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -25,6 +25,8 @@ public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
 
+    private final ItemValidator itemValidator;
+
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -165,7 +167,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         /*
@@ -193,6 +195,24 @@ public class ValidationItemControllerV2 {
                 bindingResult.reject("totalPriceMin", new Object[]{10000}, null);
             }
         }
+
+        //검증을 거치지 못한 필드 또는 복합 룰이 하나라도 존재하는 경우 다시 입력 폼 렌더링
+        if (bindingResult.hasErrors()) {
+            log.info("errors={} ", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        //검증을 성공적으로 거쳤을 때의 실행 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        itemValidator.validate(item, bindingResult);
 
         //검증을 거치지 못한 필드 또는 복합 룰이 하나라도 존재하는 경우 다시 입력 폼 렌더링
         if (bindingResult.hasErrors()) {
